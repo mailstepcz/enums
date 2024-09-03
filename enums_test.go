276bee7081084167698253b1e000fd006291edbb
Petr Homola <homola.p@gmail.com>
2024-09-03 15:55:54 +0100

    initial

---
+++ enums_test.go
@@ -0,0 +1,63 @@
+package enums
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/require"
+)
+
+type AbcEnum String
+
+const (
+	A1 AbcEnum = "a1"
+	B2 AbcEnum = "b2"
+	C3 AbcEnum = "c3"
+)
+
+var abcEnum = NewClosedEnum(A1, B2, C3)
+
+var _ ClosedEnum = AbcEnum("")
+
+func (v AbcEnum) EnumValueIsValid() bool {
+	_, ok := EnumGet[AbcEnum](&abcEnum, string(v))
+	return ok
+}
+
+func (v AbcEnum) DefaultValue() string {
+	return string(abcEnum.DefaultValue())
+}
+
+func (v AbcEnum) Value() (driver.Value, error) {
+	return string(v), nil
+}
+
+func TestClosedEnum(t *testing.T) {
+	r := require.New(t)
+
+	s, ok := EnumGet[AbcEnum](&abcEnum, "a1")
+	r.True(ok)
+	r.Equal("a1", string(s))
+
+	s, ok = EnumGet[AbcEnum](&abcEnum, "d4")
+	r.False(ok)
+	r.Equal("", string(s))
+}
+
+func TestEnumInterned(t *testing.T) {
+	r := require.New(t)
+
+	s1 := fmt.Sprintf("a%d", 1)
+	s2 := fmt.Sprintf("a%d", 1)
+
+	r.True(unsafe.StringData(s1) != unsafe.StringData(s2))
+
+	e1, ok := EnumGet[AbcEnum](&abcEnum, s1)
+	r.True(ok)
+	e2, ok := EnumGet[AbcEnum](&abcEnum, s2)
+	r.True(ok)
+
+	r.True(unsafe.StringData(string(e1)) == unsafe.StringData(string(e2)))
+}
